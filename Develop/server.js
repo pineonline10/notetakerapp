417c80903558a0d95ccde199891ecaed81f127e7
pineonline10 <joshisstillpine@gmail.com>
2023-10-08 16:17:17 -0400

    changes routes and paths, adds functionality

--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
+const { v4: uuidv4 } = require('uuid'); // Add this line
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -9,52 +10,31 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './Develop/public/notes.html')));
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, './Develop/public/index.html')));
+// HTML routes
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './public/notes.html')));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
 
+// API routes
 app.get('/api/notes', (req, res) => {
-  fs.readFile('db.json', 'utf8', (err, data) => {
-    if (err) throw err;
-    res.json(JSON.parse(data));
-  });
+
+  const saveNotes = JSON.parse(fs.readFileSync(path.join(__dirname, './db/db.json'), 'utf8'));
+  res.json(saveNotes);
 });
 
 app.post('/api/notes', (req, res) => {
-  const newNote = req.body;
+  const saveNotes = JSON.parse(fs.readFileSync(path.join(__dirname, './db/db.json'), 'utf8'));
+  const newNote = { ...req.body, id: uuidv4() };
+  saveNotes.push(newNote);
+  fs.writeFileSync(path.join(__dirname, './db/db.json'), JSON.stringify(saveNotes));
+  res.json({ message: "You've saved a note!" });
+});
 
-// Read notes
-app.get('/api/notes', (req, res) => {
-    const saveNotes = JSON.parse(fs.readFileSync(path.join(__dirname, './db/db.json')));
-    res.json(saveNotes);
-  });
-  
-  // Create new note
-  app.post('/api/notes', (req, res) => {
-    const saveNotes = JSON.parse(fs.readFileSync(path.join(__dirname, './db/db.json')));
-    saveNotes.push({ ...req.body, id: uuidv4() });
-    fs.writeFileSync(path.join(__dirname, './db/db.json'), JSON.stringify(saveNotes));
-    res.json({ message: "You've saved a note!" });
-  });
-  
-  // Delete note
-  app.delete('/api/notes/:id', (req, res) => {
-    const id = req.params.id;
-    let saveNotes = JSON.parse(fs.readFileSync(path.join(__dirname, './db/db.json')));
-    saveNotes = saveNotes.filter(note => note.id !== id);
-    fs.writeFileSync(path.join(__dirname, './db/db.json'), JSON.stringify(saveNotes));
-    res.json({ message: 'Note deleted!' });
-  });
-  
-
-  fs.readFile('db.json', 'utf8', (err, data) => {
-    if (err) throw err;
-    const notes = JSON.parse(data);
-    notes.push(newNote);
-    fs.writeFile('db.json', JSON.stringify(notes), (err) => {
-      if (err) throw err;
-      res.status(200).end();
-    });
-  });
+app.delete('/api/notes/:id', (req, res) => {
+  const id = req.params.id;
+  let saveNotes = JSON.parse(fs.readFileSync(path.join(__dirname, './db/db.json'), 'utf8'));
+  saveNotes = saveNotes.filter(note => note.id !== id);
+  fs.writeFileSync(path.join(__dirname, './db/db.json'), JSON.stringify(saveNotes));
+  res.json({ message: 'Note deleted!' });
 });
 
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
